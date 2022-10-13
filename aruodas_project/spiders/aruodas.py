48f17f73baf5eef314575234b634fc58a946ac8a
Darius <darius.kuizinass@gmail.com>
2022-10-13 18:26:35 +0300

    some more pageMethods from other workplace

--- aruodas_project/spiders/aruodas.py
+++ aruodas_project/spiders/aruodas.py
@@ -1,6 +1,7 @@
 from asyncio import coroutines
 from email import header
 from importlib.resources import path
+from multiprocessing.sharedctypes import Value
 from queue import Full
 import scrapy
 from scrapy.selector import Selector
@@ -20,21 +21,23 @@ class AruodasSpider(scrapy.Spider):
                 "playwright_page_methods": [
                     PageMethod("wait_for_selector", "button#onetrust-accept-btn-handler"),
                     PageMethod("click", "button#onetrust-accept-btn-handler"),
+                    PageMethod("click", "#quickValue_FRoomNum_2"),
+                    PageMethod("click", "#display_text_obj"),
+                    PageMethod("screenshot", path="view.png"),
                 ]
             }
             )
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-
-        
+    
         html_body = await page.inner_html("body")
         await page.close()
         
         body = Selector(text=html_body)
-        # yield {
-        #     '//*[@id="onetrust-banner-sdk"]': response.xpath("//*[@id='onetrust-banner-sdk']/text()").getall(),
-        #     "all_buttons" : response.xpath('//button/text()').getall()
-        # }
+        yield {
+        #     "div.search-form-content" : response.css("body div.main.filter-form div.header-strip div.header-filter-strip div.header-filter form div.filter-col div.search-form-content").get(),
+        #     "div#searchFormContainer" : response.css("body div.main.filter-form div.header-strip div.header-filter-strip div.header-filter form div.filter-col div.search-form-content div#searchFormContainer").get()
+         }
          
     
