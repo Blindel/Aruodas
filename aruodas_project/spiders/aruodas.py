5bac10b1bd11cea811bb115a096a07234802dea5
Darius <darius.kuizinass@gmail.com>
2022-10-19 08:32:53 +0300

    seems it worked

--- aruodas_project/spiders/aruodas.py
+++ aruodas_project/spiders/aruodas.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-from asyncio import coroutines
-from email import header
-from importlib.resources import path
-=======
 from email import header
 from importlib.resources import path
 from multiprocessing.sharedctypes import Value
->>>>>>> parent of b287fff... god dammit
 from queue import Full
 import scrapy
 from scrapy.selector import Selector
@@ -26,56 +20,27 @@ class AruodasSpider(scrapy.Spider):
                 "playwright_page_methods": [
                     PageMethod("wait_for_selector", "button#onetrust-accept-btn-handler"),
                     PageMethod("click", "button#onetrust-accept-btn-handler"),
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
                     PageMethod("click", "#quickValue_FRoomNum_2"),
                     PageMethod("click", "#display_text_obj"),
                     PageMethod("screenshot", path="view.png"),
-=======
+
                     PageMethod("click", "span#display_text_obj"),
                     PageMethod("click", "label.dropDownLabel[for=input_obj_2]"),
                     PageMethod("click", "span#display_text_FRegion"),
                     PageMethod("click", "label.dropDownLabel[for=input_FRegion_461]"),
-
->>>>>>> a6e9858b7e20d5947ef38337bce8b67d61ceb491
->>>>>>> parent of b287fff... god dammit
                 ]
             }
             )
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-<<<<<<< HEAD
 
-        
-=======
-<<<<<<< HEAD
-    
-=======
-        
->>>>>>> a6e9858b7e20d5947ef38337bce8b67d61ceb491
->>>>>>> parent of b287fff... god dammit
         html_body = await page.inner_html("body")
         await page.close()
         
         body = Selector(text=html_body)
-<<<<<<< HEAD
-        # yield {
-        #     '//*[@id="onetrust-banner-sdk"]': response.xpath("//*[@id='onetrust-banner-sdk']/text()").getall(),
-        #     "all_buttons" : response.xpath('//button/text()').getall()
-        # }
-=======
-<<<<<<< HEAD
-        yield {
-        #     "div.search-form-content" : response.css("body div.main.filter-form div.header-strip div.header-filter-strip div.header-filter form div.filter-col div.search-form-content").get(),
-        #     "div#searchFormContainer" : response.css("body div.main.filter-form div.header-strip div.header-filter-strip div.header-filter form div.filter-col div.search-form-content div#searchFormContainer").get()
-         }
-=======
+
         # yield {
             
         # }
->>>>>>> a6e9858b7e20d5947ef38337bce8b67d61ceb491
->>>>>>> parent of b287fff... god dammit
-         
-    
